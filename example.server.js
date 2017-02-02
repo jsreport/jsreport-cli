7eeb34ba040a1be8cbc8889d2053a7dd7b7248d2
BJR Matos <bjrmatos@gmail.com>
2017-02-02 11:56:39 -0500

    add jsreport.entryPoint and jsreport instance export in init command

--- example.server.js
+++ example.server.js
@@ -1,9 +1,14 @@
 var jsreport = require('$moduleName$')()
 
-jsreport.init().then(function () {
-  // running
-}).catch(function (e) {
-  // error during startup
-  console.error(e.stack)
-  process.exit(1)
-})
+if (require.main !== module) {
+  // export jsreport instance to make it possible to use jsreport-cli
+  module.exports = jsreport
+} else {
+  jsreport.init().then(function () {
+    // running
+  }).catch(function (e) {
+    // error during startup
+    console.error(e.stack)
+    process.exit(1)
+  })
+}
