2425ab13780212a975c30eebba761ca4e083f467
BJR Matos <bjrmatos@gmail.com>
2017-02-15 22:15:25 -0500

    refactoring cli handling for unit testing

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ var path = require('path')
 var semver = require('semver')
 var Liftoff = require('liftoff')
 var createCommandParser = require('./lib/createCommandParser')
+var commander = require('./lib/commander')
 var init = require('./lib/commands/init')
 var repair = require('./lib/commands/repair')
 var cliPackageJson = require('./package.json')
@@ -19,37 +20,47 @@ var cli = new Liftoff({
 cli.launch({}, initCLI)
 
 function initCLI (env) {
-  var parseCommands
+  var args = process.argv.slice(2)
+  var cwd = process.cwd()
+  var localCommander
 
   if (!env.modulePath) {
     // if no local installation is found,
     // try to detect if some global command was specified
-    var globalCliHandler = (
-      createCommandParser([init, repair])
-      .argv
-    )
+    var globalCliHandler = commander(cwd, {
+      builtInCommands: [init, repair],
+      ignoreEntryPointCommands: ['init', 'repair'],
+      cli: createCommandParser()
+    })
 
-    if (globalCliHandler._.length > 0) {
-      if (COMMANDS_AVAILABLE_GLOBALLY.indexOf(globalCliHandler._[0]) !== -1) {
-        return
+    globalCliHandler.on('started', function (err, info) {
+      if (err) {
+        console.error(err)
+        return process.exit(1)
       }
+
+      if (!info.handled) {
+        console.error('Local jsreport-cli not found in:', env.cwd)
+        console.error('Try installing jsreport-cli or jsreport package')
+
+        return process.exit(1)
+      }
+
+      return
+    })
+
+    globalCliHandler.start(args)
+  } else {
+    // Check for semver difference between global cli and local installation
+    if (semver.gt(cliPackageJson.version, env.modulePackage.version)) {
+      console.log('Warning: jsreport-cli version mismatch:')
+      console.log('Global jsreport-cli is', cliPackageJson.version)
+      console.log('Local jsreport-cli is', env.modulePackage.version)
     }
 
-    console.error('Local jsreport-cli not found in:', env.cwd)
-    console.error('Try installing jsreport-cli or jsreport package')
+    localCommander = require(path.join(path.dirname(env.modulePath), 'lib/commander.js'))(cwd)
 
-    return process.exit(1)
+    // start processing
+    localCommander.start(args)
   }
-
-  // Check for semver difference between global cli and local installation
-  if (semver.gt(cliPackageJson.version, env.modulePackage.version)) {
-    console.log('Warning: jsreport-cli version mismatch:')
-    console.log('Global jsreport-cli is', cliPackageJson.version)
-    console.log('Local jsreport-cli is', env.modulePackage.version)
-  }
-
-  parseCommands = require(path.join(path.dirname(env.modulePath), 'lib/parseCommands.js'))
-
-  // start parsing
-  parseCommands(process.argv.slice(2), process.cwd())
 }
