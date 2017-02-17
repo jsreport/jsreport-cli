5067ff83e0379bbb833aa2c9d4b5cb3ce4e6be90
BJR Matos <bjrmatos@gmail.com>
2017-02-17 18:06:43 -0500

    add tests for uninstall command

---
+++ test/commands/uninstall.spec.js
@@ -0,0 +1,139 @@
+'use strict'
+
+var path = require('path')
+var fs = require('fs')
+var childProcess = require('child_process')
+var should = require('should')
+var utils = require('../utils')
+var uninstall = require('../../lib/commands/uninstall').handler
+
+var IS_WINDOWS = process.platform === 'win32'
+
+var TEMP_DIRS = [
+  'uninstall-empty',
+  'uninstall-packagejson-only',
+  'uninstall-packagejson-ok'
+]
+
+describe('uninstall command', function () {
+  // disabling timeout because removing files could take a
+  // couple of seconds
+  this.timeout(0)
+
+  before(function () {
+    utils.cleanTempDir(TEMP_DIRS)
+
+    utils.createTempDir(TEMP_DIRS, function (dir, absoluteDir) {
+      switch (dir) {
+        case 'uninstall-packagejson-only':
+          fs.writeFileSync(
+            path.join(absoluteDir, './package.json'),
+            JSON.stringify({
+              description: 'example project'
+            }, null, 2)
+          )
+          return
+
+        case 'uninstall-packagejson-ok':
+          fs.writeFileSync(
+            path.join(absoluteDir, './package.json'),
+            JSON.stringify({
+              name: 'jsreport-server-for-uninstall',
+              main: 'server.js',
+              dependencies: {
+                jsreport: '*'
+              }
+            }, null, 2)
+          )
+
+          fs.writeFileSync(
+            path.join(absoluteDir, './server.js'),
+            'require("jsreport")().init().catch(function(err) { console.error("Error starting jsreport:", err); process.exit(1); })'
+          )
+          return
+      }
+    })
+  })
+
+  it('should not work on empty directory', function () {
+    var dir = utils.getTempDir('uninstall-empty')
+    var uninstallAsync = uninstall({ context: { cwd: dir } })
+
+    if (!IS_WINDOWS) {
+      should(uninstallAsync).be.fulfilledWith({ uninstalled: false, serviceName: null })
+    } else {
+      should(uninstallAsync).be.rejected()
+    }
+  })
+
+  it('should not work on directory with package.json without name field', function () {
+    var dir = utils.getTempDir('uninstall-packagejson-only')
+    var uninstallAsync = uninstall({ context: { cwd: dir } })
+
+    if (!IS_WINDOWS) {
+      should(uninstallAsync).be.fulfilledWith({ uninstalled: false, serviceName: null })
+    } else {
+      should(uninstallAsync).be.rejected()
+    }
+  })
+
+  it('should uninstall windows service', function (done) {
+    // disabling timeout because npm install could take a
+    // couple of minutes
+    this.timeout(0)
+
+    var dir = utils.getTempDir('uninstall-packagejson-ok')
+    var uninstallAsync
+
+    if (!IS_WINDOWS) {
+      return done()
+    }
+
+    console.log('installing jsreport for the test case...')
+
+    childProcess.exec('npm install', {
+      cwd: dir
+    }, function (error, stdout, stderr) {
+      var pathToWinser = path.join(__dirname, '../../node_modules/.bin/winser.cmd')
+
+      if (error) {
+        return done(error)
+      }
+
+      console.log('installing app in', dir, 'as windows service for the test case..')
+
+      childProcess.exec('"' + pathToWinser + '" -i -p "' + dir + '"', {
+        cwd: dir
+      }, function (error) {
+        if (error) {
+          return done(error)
+        }
+
+        uninstallAsync = uninstall({ context: { cwd: dir } })
+
+        uninstallAsync
+        .then(function (serviceInfo) {
+          if (!IS_WINDOWS) {
+            should(serviceInfo.uninstalled).be.eql(false)
+            return done()
+          }
+
+          childProcess.exec('sc query "' + serviceInfo.serviceName + '"', {
+            cwd: dir
+          }, function (error, stdout) {
+            if (error) {
+              return done()
+            }
+
+            return done(new Error('service ' + serviceInfo.serviceName + ' should have been uninstalled'))
+          })
+        })
+        .catch(done)
+      })
+    })
+  })
+
+  after(function () {
+    utils.cleanTempDir(TEMP_DIRS)
+  })
+})
